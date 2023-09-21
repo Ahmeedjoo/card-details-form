e54582145889eb945a9f462dba2a5d4ffb75f6dd
ahmedjoo <ahmedjoo2003@gmail.com>
2023-09-21 04:54:11 +0300

    fix some UX bugs

--- index.js
+++ index.js
@@ -15,16 +15,17 @@ let cardName = document.getElementById("cardName"),
   cvcWrong = document.getElementById("cvcWrong"),
   form = document.getElementById("form"),
   nameWrong = document.getElementById("nameWrong"),
-  complate = document.getElementById("complate")
-;
-
+  complate = document.getElementById("complate"),
+  cardBack = document.getElementById("cardBack"),
+  card = document.getElementById("card"),
+  reloadBtn = document.getElementById("reload-btn");
 let wrong = false;
 
 function inputCardInfo() {
-  cardUserName.innerHTML = `${cardName.value}`
+  cardUserName.innerHTML = `${cardName.value}`;
   if (cardNumber.value == Number(cardNumber.value)) {
-    if(cardName.value != ''){
-      nameWrong.innerHTML = ''
+    if (cardName.value != "") {
+      nameWrong.innerHTML = "";
     }
     var x = cardNumber.value;
     const str_a = x.toString();
@@ -36,84 +37,102 @@ function inputCardInfo() {
       "  " +
       Number(str_a.slice(8, 12)) +
       "  " +
-      Number(str_a.slice(12, 16))
-    ;
+      Number(str_a.slice(12, 16));
     cardNumber.classList.remove("wrong-number");
-    wrongNum.innerHTML = ""  ;
+    wrongNum.innerHTML = "";
+    wrong = false;
 
     if (mm.value == Number(mm.value) && yy.value == Number(yy.value)) {
-      expWrong.innerText = ""
-      if(Number(mm.value) < 10){
-        mSpan.innerHTML = `0${mm.value}`
-      }else if(Number(mm.value) > 12){
-        console.log("undifind date")
+      expWrong.innerText = "";
+      if (Number(mm.value) < 10) {
+        mSpan.innerHTML = `0${mm.value}`;
+      } else if ((mm.value == '0')) {
+        mm.classList.add("redBorder");
+        console.log("undifind date");
         wrong = true;
-        expWrong.innerText = "coudn`t be blank"
-        mm.classList.add("redBorder")
-      }
-      else{
-        mSpan.innerHTML = `${mm.value}`
-        expWrong.innerText = ""
-        mm.classList.remove("redBorder")
-      }
-      if(Number(yy.value) > 30 ){
+        expWrong.innerText = "coudn`t be blank";
+      } else if (Number(mm.value) > 12) {
+        console.log("undifind date");
         wrong = true;
-        expWrong.innerText = "coudn`t be blank"
-        yy.classList.add("redBorder")
-      }else if(Number(yy.value) < 23  ){
-        expWrong.innerText = "coudn`t be blank"
+        expWrong.innerText = "coudn`t be blank";
+        mm.classList.add("redBorder");
       } else {
-        ySpan.innerHTML = `${yy.value}`
-        expWrong.innerText = ""
-        yy.classList.remove("redBorder")
+        mSpan.innerHTML = `${mm.value}`;
+        expWrong.innerText = "";
+        mm.classList.remove("redBorder");
       }
-    }
-    else{
-      expWrong.innerText = "coudn`t be blank"
+
+      if (Number(yy.value) > 30) {
+        wrong = true;
+        expWrong.innerText = "coudn`t be blank";
+        yy.classList.add("redBorder");
+      } else if (Number(yy.value) < 23) {
+        expWrong.innerText = "coudn`t be blank";
+        wrong = true;
+      } else {
+        ySpan.innerHTML = `${yy.value}`;
+        expWrong.innerText = "";
+        yy.classList.remove("redBorder");
+      }
+    } else {
+      expWrong.innerText = "coudn`t be blank";
       wrong = true;
     }
-  }else {
+  } else {
     cardNumber.classList.add("wrong-number");
     wrongNum.innerHTML = "Wrong format , numbers only";
     wrong = true;
   }
 
-  if (cvc.value == Number(cvc.value)){
-    cardCvc.innerHTML = `${Number(cvc.value)}`
-    cvcWrong.innerText = ""
-    cvc.classList.remove("redBorder")
-  }
-  else{
+  if (cvc.value == Number(cvc.value)) {
+    cardCvc.innerHTML = `${Number(cvc.value)}`;
+    cvcWrong.innerText = "";
+    cvc.classList.remove("redBorder");
+  } else {
     wrong = true;
-    cvcWrong.innerText = "coudn`t be blank"
-    cvc.classList.add("redBorder")
+    cvcWrong.innerText = "coudn`t be blank";
+    cvc.classList.add("redBorder");
   }
 }
-Confiemm.addEventListener('click' , function(){
-  if(wrong == true){
-    console.log("wrong input info");
-  } else  if(mm.value == ''&& yy.value == ''){
-    expWrong.innerHTML = 'Inter Exp Info, Please!'
-  }else if(cardName.value == ''){
-    nameWrong.innerHTML = 'Inter card name, please'
-  }else if(cardNumber.value == ''){
-    wrongNum.innerHTML = "Inter card number, please"
-  }else if(cvc.value == ''){
-    cvcWrong.innerHTML = "Inter card cvc, please"
-  }
-  
-  if(wrong != true && 
-    cvc.value != '' &&
-    cardNumber.value != '' &&
-    cardName.value != '' &&
-    mm.value != ''&& yy.value != ''){
 
-    form.style.display = "none"
-    complate.style.display = "flex"
-    cardNumTxt.innerHTML = "0000 0000 0000 0000"
-    cardUserName.innerHTML = "Ahmed Yousef"
-    mSpan.innerHTML = "00"
-    ySpan.innerHTML = "00"
-    cardCvc.innerHTML = ""
+function confirmData() {
+  if (wrong === true) {
+    console.log("wrong input info");
+  } else if (mm.value == "" && yy.value == "") {
+    expWrong.innerHTML = "Inter Exp Info, Please!";
+  } else if (cardName.value == "") {
+    nameWrong.innerHTML = "Inter card name, please";
+  } else if (cardNumber.value == "") {
+    wrongNum.innerHTML = "Inter card number, please";
+  } else if (cvc.value == "") {
+    cvcWrong.innerHTML = "Inter card cvc, please";
+  } else if (
+    wrong != true &&
+    cvc.value != "" &&
+    cardNumber.value != "" &&
+    cardName.value != "" &&
+    mm.value != "" &&
+    yy.value != ""
+  ) {
+    form.style.display = "none";
+    complate.style.display = "flex";
+    cardNumTxt.innerHTML = "0000 0000 0000 0000";
+    cardUserName.innerHTML = "Ahmed Yousef";
+    mSpan.innerHTML = "00";
+    ySpan.innerHTML = "00";
+    cardCvc.innerHTML = "";
+  } else {
+    console.log("Complate Your card data, Please ! " + "(;");
   }
-})
+}
+
+Confiemm.addEventListener("click", confirmData);
+
+window.onload = () => {
+  card.classList.add("show");
+  cardBack.classList.add("show");
+};
+
+reloadBtn.addEventListener("click", () => {
+  window.location.reload();
+});
